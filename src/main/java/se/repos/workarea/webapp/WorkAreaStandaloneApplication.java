d6d37ad1f05774900426eb21973584ad4a156de7
hsvallfors <hsvallfors>
2013-12-09 11:01:31 +0000

    Deleted most of CTKs contributions, kept some stubs.

--- src/main/java/se/repos/workarea/webapp/WorkAreaStandaloneApplication.java
+++ src/main/java/se/repos/workarea/webapp/WorkAreaStandaloneApplication.java
@@ -8,30 +8,28 @@ import java.util.Set;
 
 import javax.ws.rs.core.Application;
 
+import se.repos.lgr.Lgr;
+import se.repos.lgr.LgrFactory;
+
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 
-import se.repos.lgr.Lgr;
-import se.repos.lgr.LgrFactory;
-import se.repos.workarea.rest.WorkAreaResource;
-
 public class WorkAreaStandaloneApplication extends Application {
 
-	public static final Lgr logger = LgrFactory.getLogger();
+    public static final Lgr logger = LgrFactory.getLogger();
 
-	Injector configure() {
-		return Guice.createInjector(
-				new WorkAreaModule()
-				);
-	}
-	
-	@Override
-	public Set<Object> getSingletons() {
-		logger.info("Starting application");
-		Injector context = configure();
-		Set<Object> resources = new HashSet<Object>();
-		resources.add(context.getInstance(WorkAreaResource.class));
-		return resources;
-	}
-	
+    private static Injector configure() {
+        return Guice.createInjector(new WorkAreaModule());
+    }
+
+    @Override
+    public Set<Object> getSingletons() {
+        logger.info("Starting application");
+        Injector context = WorkAreaStandaloneApplication.configure();
+        Set<Object> resources = new HashSet<>();
+        // TODO Add resources.
+        // resources.add(context.getInstance(WorkAreaResource.class));
+        return resources;
+    }
+
 }
